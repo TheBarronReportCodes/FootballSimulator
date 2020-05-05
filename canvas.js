77de446e977a27509eff1556437c318f327b9425
Jordan Barron <jordanbarron1531@gmail.com>
2020-05-05 00:00:44 -0400

    practice with drawing of random circles

--- canvas.js
+++ canvas.js
@@ -4,10 +4,24 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var paintBrush = canvas.getContext("2d");
-paintBrush.strokeRect(300, 300, 200, 100);
+paintBrush.fillStyle = "red";
+paintBrush.fillRect(20, 20, 80, 80);
+paintBrush.fillStyle = "green";
+paintBrush.fillRect(420, 100, 80, 80);
+paintBrush.fillStyle = "blue";
+paintBrush.fillRect(620, 300, 80, 80);
 
-paintBrush.arc(400, 400, 30, 0, Math.PI * 2, false);
+paintBrush.arc(200, 200, 40, 0, Math.PI * 2, false);
 paintBrush.strokeStyle = "purple";
 paintBrush.stroke();
+
+for (var current = 0; current < 1000; current++) {
+  paintBrush.beginPath();
+  randomX = Math.random() * 600;
+  randomY = Math.random() * 600;
+  paintBrush.arc(randomX, randomY, 40, 0, Math.PI * 2, false);
+  paintBrush.strokeStyle = "purple";
+  paintBrush.stroke();
+}
 
 console.log(arc);
