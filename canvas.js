7a61d390b2eb119aa352d927a01009512e872e73
Jordan Barron <jordanbarron1531@gmail.com>
2020-05-04 20:32:36 -0400

    testing shapes

--- canvas.js
+++ canvas.js
@@ -2,4 +2,8 @@ var canvas = document.querySelector("canvas");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+var shapes = canvas.getContext("2d");
+shapes.fillRect(300, 300, 200, 100);
+
 console.log(canvas);
