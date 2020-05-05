6d573059e07fb936c10822edbe307415fa3a80c1
Jordan Barron <jordanbarron1531@gmail.com>
2020-05-04 23:44:06 -0400

    drawing first circle

--- canvas.js
+++ canvas.js
@@ -3,7 +3,11 @@ var canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var shapes = canvas.getContext("2d");
-shapes.fillRect(300, 300, 200, 100);
+var paintBrush = canvas.getContext("2d");
+paintBrush.strokeRect(300, 300, 200, 100);
 
-console.log(canvas);
+paintBrush.arc(400, 400, 30, 0, Math.PI * 2, false);
+paintBrush.strokeStyle = "purple";
+paintBrush.stroke();
+
+console.log(arc);
